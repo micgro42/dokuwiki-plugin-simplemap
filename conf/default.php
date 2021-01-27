fb0f3d3cb839a4c7e7c827984ac1d3db4392c7c4
Michael Große <mic.grosse@googlemail.com>
2021-01-27 11:19:23 +0100

    🔥 refactor: remove unused code

--- conf/default.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Default settings for the simplemap plugin
- *
- * @author Michael Große <mic.grosse@googlemail.com>
- */
-
-//$conf['fixme']    = 'FIXME';
--- conf/metadata.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Options for the simplemap plugin
- *
- * @author Michael Große <mic.grosse@googlemail.com>
- */
-
-//$meta['fixme'] = array('string');
