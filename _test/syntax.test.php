4a958fc6e819dfb815bc9d06a4a229b3aaec255d
Michael Große <mic.grosse@googlemail.com>
2021-01-27 12:09:01 +0100

    ✅ test: add test for syntax render method

--- _test/syntax.test.php
+++ _test/syntax.test.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace dokuwiki\plugin\simplemap\test;
 
 use Doku_Handler;
+use dokuwiki\test\mock\Doku_Renderer;
 use syntax_plugin_simplemap;
 use DokuWikiTest;
 
@@ -48,5 +49,38 @@ class simplemap_syntax_test extends DokuWikiTest {
 
         // assert
         self::assertEquals($expected_output, $actual_output, $msg);
+    }
+
+    public function test_rendererXHTML() {
+        /** @var syntax_plugin_simplemap $syntax */
+        $syntax = plugin_load('syntax', 'simplemap');
+        $testData = [
+            'type' => 'osm',
+            'lat' => '50.234',
+            'long' => '13.123',
+        ];
+        $mockRenderer = new Doku_Renderer();
+
+        $actualStatus = $syntax->render('xhtml', $mockRenderer, $testData);
+
+        self::assertTrue($actualStatus);
+        $expectedHTML = '<iframe width="425" height="350" src="https://www.openstreetmap.org/export/embed.html?bbox=13.119%2C50.232%2C13.127%2C50.236&amp;layer=mapnik&marker=50.234%2C13.123"></iframe><br><a href="https://www.openstreetmap.org/#map=14/50.234/13.123" target="_blank">View Larger Map</a>';
+        self::assertSame($expectedHTML, $mockRenderer->doc);
+    }
+
+    public function test_rendererMeta() {
+        /** @var syntax_plugin_simplemap $syntax */
+        $syntax = plugin_load('syntax', 'simplemap');
+        $testData = [
+            'type' => 'osm',
+            'lat' => '50.234',
+            'long' => '13.123',
+        ];
+        $mockRenderer = new Doku_Renderer();
+
+        $actualStatus = $syntax->render('meta', $mockRenderer, $testData);
+
+        self::assertFalse($actualStatus);
+        self::assertSame('', $mockRenderer->doc);
     }
 }
